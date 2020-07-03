c982b15f2c7d95ad9f8fbd0ee8186aae8bc303ba
gatenosixLee <gatenosix@gmail.com>
2020-07-03 16:58:57 +0000

    16x2에 맞도록 수정

--- hello_world.py
+++ hello_world.py
@@ -17,6 +17,6 @@ lcd.display_string("Hello ODROID", 2)
 while True:
   dateString = datetime.now().strftime('%b %d %y')
   timeString = datetime.now().strftime('%H:%M:%S')
-  lcd.display_string(dateString, 3)
-  lcd.display_string(timeString, 4)
+#   lcd.display_string(dateString, 3)
+#   lcd.display_string(timeString, 4)
   sleep(1)
